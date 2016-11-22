c904dac934ff33658f0fb8e7e731121f8d05ff39
DESKTOP-GDKUE2G\Natalia <natala-jozwiak@wp.pl>
2016-11-22 23:48:00 +0100

    init

--- app/src/main/java/com/example/natalia/super_inzynierka/Start.java
+++ app/src/main/java/com/example/natalia/super_inzynierka/Start.java
@@ -3,9 +3,7 @@ package com.example.natalia.super_inzynierka;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -18,44 +16,55 @@ public class Start extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-//        String url = "http://natalia123.pythonanywhere.com/spy_app/list/";
+        String url = "http://natalia123.pythonanywhere.com/spy_app/list/";
 
-
-//        try {
-//            URLConnection connection = new URL(url).openConnection();
-//            System.out.println("dziala");
-//        } catch (IOException e) {
-//            System.out.println("nie dzisla");
-//        }
+        try {
+            URLConnection connection = new URL(url).openConnection();
+            System.out.println("dziala");
+        } catch (IOException e) {
+            System.out.println("nie dzisla");
+        }
 //        try {
 //            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
 //            readStream(in);
+//        } catch (IOException e) {
+//            e.printStackTrace();
 //        } finally {
 //            urlConnection.disconnect();
 //        }
 
 
-        URL url = null;
-        try {
-            url = new URL("http://www.android.com/");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        HttpURLConnection urlConnection = null;
-        try {
-            urlConnection = (HttpURLConnection) url.openConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-            readStream(in);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            urlConnection.disconnect();
-        }
+//        URL url = null;
+//        try {
+//            url = new URL("http://www.android.com/");
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+//        HttpURLConnection urlConnection = null;
+//        try {
+//            urlConnection = (HttpURLConnection) url.openConnection();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+//            readStream(in);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            urlConnection.disconnect();
+//        }
 
+    }
+
+    private String readStream(InputStream is) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader r = new BufferedReader(new InputStreamReader(is),1000);
+        for (String line = r.readLine(); line != null; line =r.readLine()){
+            sb.append(line);
+        }
+        is.close();
+        return sb.toString();
     }
 
 

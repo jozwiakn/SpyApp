bce4204816a86ab0133ced7819879e09794e695a
Łukasz Woźny <tamburyniarz@gmail.com>
2016-11-23 20:23:58 +0100

    Connection succesfull

--- app/src/main/java/com/example/natalia/super_inzynierka/Start.java
+++ app/src/main/java/com/example/natalia/super_inzynierka/Start.java
@@ -2,14 +2,10 @@ package com.example.natalia.super_inzynierka;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
+import android.view.View;
+import android.widget.TextView;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
 public class Start extends AppCompatActivity {
 
@@ -18,44 +14,33 @@ public class Start extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-//        String url = "http://natalia123.pythonanywhere.com/spy_app/list/";
+    }
 
+    public void connect(View view) {
+//        TextView displayTextView = (TextView) findViewById(R.id.text_super);
+//        displayTextView.setText("Connect click");
 
-//        try {
-//            URLConnection connection = new URL(url).openConnection();
-//            System.out.println("dziala");
-//        } catch (IOException e) {
-//            System.out.println("nie dzisla");
-//        }
-//        try {
-//            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-//            readStream(in);
-//        } finally {
-//            urlConnection.disconnect();
-//        }
+        String url = "http://natalia123.pythonanywhere.com/spy_app/list/";
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody) {
+                if (responseBody == null) { /* empty response, alert something*/ return; }
+                //success response, do something with it!
+                String response = new String(responseBody);
+                TextView displayTextView = (TextView) findViewById(R.id.text_super);
+                displayTextView.setText(response);
+            }
 
-
-        URL url = null;
-        try {
-            url = new URL("http://www.android.com/");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        HttpURLConnection urlConnection = null;
-        try {
-            urlConnection = (HttpURLConnection) url.openConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-            readStream(in);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            urlConnection.disconnect();
-        }
-
+            @Override
+            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody, Throwable error) {
+                if (responseBody == null) { /* empty response, alert something*/ return; }
+                //error response, do something with it!
+                String response = new String(responseBody);
+                TextView displayTextView = (TextView) findViewById(R.id.text_super);
+                displayTextView.setText(response);
+            }
+        });
     }
 
 

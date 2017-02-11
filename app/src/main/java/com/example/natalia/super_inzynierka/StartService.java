8fb2addfa4153f725408e59aa43bc99ae3659c0b
DESKTOP-GDKUE2G\Natalia <natala-jozwiak@wp.pl>
2017-02-11 14:07:00 +0100

    refactor StartService

--- app/src/main/java/com/example/natalia/super_inzynierka/StartService.java
+++ app/src/main/java/com/example/natalia/super_inzynierka/StartService.java
@@ -1,30 +1,16 @@
 package com.example.natalia.super_inzynierka;
 
-import android.Manifest;
 import android.app.Service;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.*;
 import android.net.Uri;
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
-import android.os.PowerManager;
-import android.provider.Settings;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
-import com.loopj.android.http.AsyncHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
-import cz.msebera.android.httpclient.Header;
-import cz.msebera.android.httpclient.io.SessionOutputBuffer;
-
-import java.io.IOException;
-import java.util.*;
 
 /**
  * Created by Natalia on 01.12.2016.
